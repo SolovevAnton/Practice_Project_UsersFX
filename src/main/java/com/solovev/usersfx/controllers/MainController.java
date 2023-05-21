da58938282d9daf766b9d58f60ab3fd487936d32
Solovev Anton <solovev.anton93@gmail.com>
2023-05-21 16:10:42 +0300

    Added user representation

--- src/main/java/com/solovev/usersfx/controllers/MainController.java
+++ src/main/java/com/solovev/usersfx/controllers/MainController.java
@@ -2,36 +2,72 @@ package com.solovev.usersfx.controllers;
 
 import com.solovev.usersfx.model.User;
 import com.solovev.usersfx.repository.UserRepository;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.util.Callback;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainController {
     @FXML
     public ComboBox<User> comboBoxUsers = new ComboBox<>();
+    public TextArea textAreaSelectedUserInfo;
+    public ListView<User> addedUsersListView;
 
     @FXML
     public void initialize() throws IOException {
-            URL urlToGetUsers = new URL("https://jsonplaceholder.typicode.com/users");
-            UserRepository userRep = new UserRepository(urlToGetUsers);
-            comboBoxUsers.getItems().setAll(userRep.getUsers());
-            Callback<ListView<User>, ListCell<User>> factory = lv -> new ListCell<User>() {
-                @Override
-                protected void updateItem(User item, boolean empty) {
-                    super.updateItem(item, empty);
-                    setText(empty ? "" : item.getName());
+        URL urlToGetUsers = new URL("https://jsonplaceholder.typicode.com/users");
+        UserRepository userRep = new UserRepository(urlToGetUsers);
+        comboBoxUsers.getItems().setAll(userRep.getUsers());
+        Callback<ListView<User>, ListCell<User>> factory = lv -> new ListCell<>() {
+            @Override
+            protected void updateItem(User item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty ? "" : item.getName());
+            }
+        };
+        comboBoxUsers.setCellFactory(factory);
+        comboBoxUsers.setButtonCell(factory.call(null));
+
+        ChangeListener<User> changeInCombo = (list, oldValue, newValue) -> {
+            if (newValue != null) {
+                textAreaSelectedUserInfo.setText(showUserInfo(newValue));
+            } else {
+                textAreaSelectedUserInfo.setText("");
+            }
+        };
+        comboBoxUsers.valueProperty().addListener(changeInCombo);
+    }
+
+    /**
+     * Method creates nice String representation of the User:
+     *
+     * @param user user to create representation
+     * @return string with nice representation
+     * "
+     */
+    private String showUserInfo(User user) {
+            int lvlCount = 0;
+            String userToString = user.toString();
+            String filtered = userToString.substring(0, userToString.length() - 1)
+                    .replaceFirst("User\\{", "")
+                    .replaceAll(" ", "");
+            StringBuilder sb = new StringBuilder();
+            for (char c : filtered.toCharArray()) {
+                sb.append(c);
+                if (c == ',') {
+                    sb.append("\n").append("\t".repeat(lvlCount));
                 }
-            };
-            comboBoxUsers.setCellFactory(factory);
-            comboBoxUsers.setButtonCell(factory.call(null));
+                if (c == '{') {
+                    sb.append("\n").append("\t".repeat(++lvlCount));
+                }
+                if (c == '}') {
+                    lvlCount--;
+                }
+            }
+            return sb.toString();
     }
 
 }
