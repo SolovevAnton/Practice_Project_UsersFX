1da5019733bbe08c4756924ad3678b143063d603
Solovev Anton <solovev.anton93@gmail.com>
2023-05-21 18:27:04 +0300

    added buttons save, info and delete functionality, ready for review

--- src/main/java/com/solovev/usersfx/App.java
+++ src/main/java/com/solovev/usersfx/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Users Loaded");
+        stage.setTitle("Users saving UI");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/solovev/usersfx/controllers/MainController.java
+++ src/main/java/com/solovev/usersfx/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.solovev.usersfx.controllers;
 import com.solovev.usersfx.model.User;
 import com.solovev.usersfx.repository.UserRepository;
 import javafx.beans.value.ChangeListener;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.util.Callback;
@@ -13,42 +14,48 @@ import java.net.URL;
 public class MainController {
     @FXML
     public ComboBox<User> comboBoxUsers = new ComboBox<>();
+    @FXML
     public TextArea textAreaSelectedUserInfo;
-    public ListView<User> addedUsersListView;
+    @FXML
+    public ListView<User> addedUsersListView = new ListView<>();
+    private final ChangeListener<User> userChangeListener = (list, oldValue, newValue) -> textAreaSelectedUserInfo.setText(newValue != null ? longUserInfo(newValue) : "");
 
+    private final Callback<ListView<User>, ListCell<User>> userToOnlyNamesAppearance = lv -> new ListCell<>() {
+        @Override
+        protected void updateItem(User user, boolean empty) {
+            super.updateItem(user, empty);
+            setText(empty ? "" : shortUserInfo(user));
+        }
+    };
     @FXML
     public void initialize() throws IOException {
         URL urlToGetUsers = new URL("https://jsonplaceholder.typicode.com/users");
         UserRepository userRep = new UserRepository(urlToGetUsers);
         comboBoxUsers.getItems().setAll(userRep.getUsers());
-        Callback<ListView<User>, ListCell<User>> factory = lv -> new ListCell<>() {
-            @Override
-            protected void updateItem(User item, boolean empty) {
-                super.updateItem(item, empty);
-                setText(empty ? "" : item.getName());
-            }
-        };
-        comboBoxUsers.setCellFactory(factory);
-        comboBoxUsers.setButtonCell(factory.call(null));
+        comboBoxUsers.setCellFactory(userToOnlyNamesAppearance);
+        comboBoxUsers.setButtonCell(userToOnlyNamesAppearance.call(null));
+        comboBoxUsers.valueProperty().addListener(userChangeListener);
 
-        ChangeListener<User> changeInCombo = (list, oldValue, newValue) -> {
-            if (newValue != null) {
-                textAreaSelectedUserInfo.setText(showUserInfo(newValue));
-            } else {
-                textAreaSelectedUserInfo.setText("");
-            }
-        };
-        comboBoxUsers.valueProperty().addListener(changeInCombo);
+        addedUsersListView.setCellFactory(userToOnlyNamesAppearance);
+    }
+
+    /**
+     * Shows id and name of the user
+     * @param user to get info about
+     * @return user info string representation
+     */
+    private String shortUserInfo(User user){
+        return String.format("id: %d, Name: %s",user.getId(),user.getName());
     }
 
     /**
      * Method creates nice String representation of the User:
      *
      * @param user user to create representation
-     * @return string with nice representation
+     * @return  with long user info representation
      * "
      */
-    private String showUserInfo(User user) {
+    private String longUserInfo(User user) {
             int lvlCount = 0;
             String userToString = user.toString();
             String filtered = userToString.substring(0, userToString.length() - 1)
@@ -70,4 +77,43 @@ public class MainController {
             return sb.toString();
     }
 
+    /**
+     * Adds selected user to the List
+     * @param actionEvent buttonSave gets clicked
+     */
+    @FXML
+    public void buttonSave(ActionEvent actionEvent) {
+        User selectedUser = comboBoxUsers.getValue();
+        if(selectedUser != null) {
+            comboBoxUsers.getItems().remove(selectedUser);
+            addedUsersListView.getItems().add(selectedUser);
+        }
+
+    }
+
+    /**
+     * Deletes selected user from the saved list
+     * @param actionEvent button delete gets clicked
+     */
+    @FXML
+    public void buttonDelete(ActionEvent actionEvent) {
+        User selectedUser = addedUsersListView.getSelectionModel().getSelectedItem();
+        addedUsersListView.getItems().remove(selectedUser);
+    }
+
+    /**
+     * Shows info about selected user
+     * @param actionEvent button info gets clicked
+     */
+    @FXML
+    public void buttonShowInfo(ActionEvent actionEvent) {
+        User selectedUser = addedUsersListView.getSelectionModel().getSelectedItem();
+        if(selectedUser != null) {
+            Alert info = new Alert(Alert.AlertType.INFORMATION);
+            info.setTitle("Info");
+            info.setHeaderText("Info about selected user: " + shortUserInfo(selectedUser));
+            info.setContentText(longUserInfo(selectedUser));
+            info.show();
+        }
+    }
 }
