869650ef73bb19765f6e12c484dbcf5a418e0d5b
Solovev Anton <solovev.anton93@gmail.com>
2023-05-21 14:19:31 +0300

    Combobox working

--- src/main/java/com/solovev/usersfx/controllers/MainController.java
+++ src/main/java/com/solovev/usersfx/controllers/MainController.java
@@ -2,28 +2,36 @@ package com.solovev.usersfx.controllers;
 
 import com.solovev.usersfx.model.User;
 import com.solovev.usersfx.repository.UserRepository;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 public class MainController {
     @FXML
-    public ComboBox<User> ComboBoxOne = new ComboBox<>();
+    public ComboBox<User> comboBoxUsers = new ComboBox<>();
 
-    public MainController() {
-        try {
+    @FXML
+    public void initialize() throws IOException {
             URL urlToGetUsers = new URL("https://jsonplaceholder.typicode.com/users");
             UserRepository userRep = new UserRepository(urlToGetUsers);
-            ComboBoxOne.getItems().setAll(userRep.getUsers());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+            comboBoxUsers.getItems().setAll(userRep.getUsers());
+            Callback<ListView<User>, ListCell<User>> factory = lv -> new ListCell<User>() {
+                @Override
+                protected void updateItem(User item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(empty ? "" : item.getName());
+                }
+            };
+            comboBoxUsers.setCellFactory(factory);
+            comboBoxUsers.setButtonCell(factory.call(null));
     }
 
 }
