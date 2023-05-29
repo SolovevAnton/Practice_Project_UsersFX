854aed98f9835febe46a88b498cd89390e83fdca
Solovev Anton <solovev.anton93@gmail.com>
2023-05-29 13:58:35 +0300

    added user info UI

---
+++ src/main/java/com/solovev/usersfx/controllers/UserInfo.java
@@ -0,0 +1,22 @@
+package com.solovev.usersfx.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class UserInfo {
+    public TextField textFieldID;
+    public TextField textFieldName;
+    public TextField textFieldUsername;
+    public TextField textFieldEmail;
+    public TextArea textAreaAddress;
+    public TextField textFieldPhone;
+    public TextField textFieldWebsite;
+    public TextArea textAreaCompany;
+
+    public void buttonClose(ActionEvent actionEvent) {
+    }
+
+    public void buttonSave(ActionEvent actionEvent) {
+    }
+}
